adfac11a7085c38c382544f8a4d7caf4bff8c150
AlexCoder13 <sesasha99@gmail.com>
2025-01-12 17:39:59 +0300

    Initial Commit

---
+++ Guess the Number/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess the Number
+//
+//  Created by Александр Семёнов on 12.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
